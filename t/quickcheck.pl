375df27219fa246688907b5ef6ed9e3b3684b76b
Michael Hendricks <michael@ndrix.org>
2013-12-17 16:44:52 -0700

    Add a few quickcheck tests
    Some of the "or" properties should really be "xor".  However, even these
    properties capture a good chunk of the behavior I care about.  I can
    clarify to xor later.

---
+++ t/quickcheck.pl
@@ -0,0 +1,33 @@
+:- use_module(library(maybe)).
+:- use_module(library(quickcheck)).
+
+% define some properties
+
+just_or_nothing(Maybe:maybe(atomic)) :-
+    ( is_just(Maybe)
+    ; is_nothing(Maybe)
+    ),
+    !.
+
+maybe_value_or_nothing(Maybe:maybe(atomic)) :-
+    ( maybe_value(Maybe, _)
+    ; is_nothing(Maybe)
+    ),
+    !.
+
+list_round_trip(L0:list(atomic)) :-
+    maybe_list(Maybe, L0),
+    maybe_list(Maybe, L1),
+
+    % same head or both empty
+    ( L0 = [H|_], L1 = [H|_]
+    ; L0 = [],    L1 = []
+    ),
+    !.
+
+
+:- use_module(library(tap)).
+
+quickcheck(just_or_nothing/1).
+quickcheck(maybe_value_or_nothing/1).
+quickcheck(list_round_trip/1).
